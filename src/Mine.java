37f3f2e8eb120994d7ed4099e9b4cde97ab71f87
Jonathan Benedek <jonathan.benedek@mce-sys.com>
2017-09-18 01:27:05 +0300

    fix size for life for L Ship

--- src/Mine.java
+++ src/Mine.java
@@ -1,6 +1,6 @@
 public class Mine extends GameTool {
     public Mine (String i_species){
-        super(i_species, "Mine" , 'M', 0, 1);
+        super(i_species, "Mine" , 'M', 0, 1, "Mine");
     }
 
      public boolean updateHitMe(Position position){
@@ -15,6 +15,6 @@ public class Mine extends GameTool {
     }
 
     public  Mine (String i_species , String i_type , char i_sign , int i_row , int i_column){
-        super( i_species, i_type , i_sign, 0 , 1);
+        super( i_species, i_type , i_sign, 0 , 1, "Mine");
     }
 }
